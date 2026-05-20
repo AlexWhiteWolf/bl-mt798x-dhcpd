142d571f46904b654679a404ddd3cb3844260497
Yuzhii0718 <yuzhii0718@outlook.com>
2026-05-20 23:36:13 +0800

    uboot: failsafe: ensure dhcpd start with httpd

--- uboot-mtk-20250711/failsafe/failsafe.c
+++ uboot-mtk-20250711/failsafe/failsafe.c
@@ -14,7 +14,9 @@
 #include <net.h>
 #include <net/mtk_tcp.h>
 #include <net/mtk_httpd.h>
+#ifdef CONFIG_MTK_DHCPD
 #include <net/mtk_dhcpd.h>
+#endif
 #ifdef CONFIG_MTK_TELNETD
 #include <net/mtk_telnetd.h>
 #endif
@@ -64,9 +66,27 @@ int __weak failsafe_write_image(const void *data, size_t size, failsafe_fw_t fw)
 	return -ENOSYS;
 }
 
+static bool services_auto_started;
+
 void schedule_hook(void)
 {
-	if (!failsafe_httpd_running)
+	bool need_poll = failsafe_httpd_running;
+
+#ifdef CONFIG_MTK_DHCPD
+	need_poll = need_poll || mtk_dhcpd_is_running();
+#endif
+
+	if (!services_auto_started && !failsafe_httpd_running) {
+		services_auto_started = true;
+#ifdef CONFIG_MTK_DHCPD
+		if (!mtk_dhcpd_is_running()) {
+			printf("Starting DHCP server...\n");
+			mtk_dhcpd_start();
+			need_poll = true;
+		}
+#endif
+	}
+	if (!need_poll)
 		return;
 
 #if defined(CONFIG_MTK_TCP)
